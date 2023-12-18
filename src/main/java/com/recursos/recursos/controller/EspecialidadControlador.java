75463dc7bc1b9755254dd7104b241ad0a0a1b7f6
CarlosSaico28 <84851722+CarlosSaico28@users.noreply.github.com>
2023-12-17 19:19:17 -0500

    COMMIT

--- src/main/java/com/recursos/recursos/controller/EspecialidadControlador.java
+++ src/main/java/com/recursos/recursos/controller/EspecialidadControlador.java
@@ -1,7 +1,6 @@
 package com.recursos.recursos.controller;
 
 import com.recursos.recursos.entity.Especialidad;
-import com.recursos.recursos.entity.registro;
 import com.recursos.recursos.services.EspecialidadServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
+
 @RestController
 @RequestMapping("/especialidad")
 @CrossOrigin("*")
